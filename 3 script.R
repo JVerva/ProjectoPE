1453f7771bb9a73e4d2b1ebb7f23bcab3a952ae5
joao vera <joaoguedessoaresvera@gmail.com>
2022-05-18 14:15:49 +0100

    exercicio 3 quase

--- 3 script.R
+++ 3 script.R
@@ -1,1 +1,38 @@
-3 
+excelFilePath <- "/home/verva/Downloads/QualidadeARO3.xlsx"
+
+library(readxl)
+Estacoes <- names(read_excel(excelFilePath,"Sheet1","A1:J1", col_types = "text", .name_repair = "minimal"))
+Estacoes <- as.vector(unlist(Estacoes))
+
+EstacoesReq <- c("Restelo", "Entrecampos")
+Indexes <- match(EstacoesReq, Estacoes)
+library(openxlsx)
+Indexes <- int2col(Indexes)
+for (i in 1:length(Indexes)){
+  Indexes[i] <- paste(Indexes[i], "1:", Indexes[i], "8785", sep = "")
+}
+for (i in 1:length(Indexes)){
+  if(i==1){
+    nozono <- read_excel(excelFilePath,"Sheet1",Indexes[i], col_types = "text")
+  }else{
+    nozono <- append(nozono,read_excel(excelFilePath,"Sheet1",Indexes[i], col_types = "text"))
+  }
+}
+
+nozono <- as.double(unlist(nozono))
+
+for(i in 1:length(EstacoesReq)){
+  if(i == 1){
+    Estacoes <- rep(EstacoesReq[i],8784)
+  }else{
+   Estacoes <- append(Estacoes, rep(EstacoesReq[i],8784)) 
+  }
+}
+rm(EstacoesReq)
+
+Estacoes<-as.factor(Estacoes)
+nozono<- nozono
+data <- data.frame(Estacoes,as.factor(nozono))
+
+
+ggplot(data , aes(x = nozono)) + geom_histogram(colour = "Green", fill = "Gray",bins = 20)
